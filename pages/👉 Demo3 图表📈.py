1ee1dea874c6b209955159769d978362168d1d16
imhoaking <50255786+imhoaking@users.noreply.github.com>
2022-07-27 02:33:04 +0800

    Update 👉 Demo3 图表📈.py

--- pages/👉 Demo3 图表📈.py
+++ pages/👉 Demo3 图表📈.py
@@ -3,10 +3,10 @@ import streamlit as st
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
+# import plotly.figure_factory as ff
 from bokeh.plotting import figure
 import graphviz
-from PIL import Image
+# from PIL import Image
 
 
 
@@ -17,21 +17,21 @@ from PIL import Image
 # st.pyplot(plt.hist(arr, bins=20))
 
 
-'# 显示plotly图'
-# Add histogram data
-x1 = np.random.randn(20) +10
-x2 = np.random.randn(20)
-x3 = np.random.randn(20) -10
-# Group data together
-hist_data = [x1, x2, x3]
-group_labels = ['学生1', '学生2', '学生3']
+# '# 显示plotly图'
+# # Add histogram data
+# x1 = np.random.randn(20) +10
+# x2 = np.random.randn(20)
+# x3 = np.random.randn(20) -10
+# # Group data together
+# hist_data = [x1, x2, x3]
+# group_labels = ['学生1', '学生2', '学生3']
 
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-  hist_data, group_labels, bin_size=[8, 9, 3])
+# # Create distplot with custom bin_size
+# fig = ff.create_distplot(
+#   hist_data, group_labels, bin_size=[8, 9, 3])
 
-# Plot!
-st.plotly_chart(fig)
+# # Plot!
+# st.plotly_chart(fig)
 
 '# 显示bokeh图'
 x = [1, 2, 3, 4, 5]
@@ -64,6 +64,15 @@ graph.edge('sleep', 'runmem')
 
 st.graphviz_chart(graph)
 
+
+# '# 显示图片'
+# image = Image.open('./pics/pic1.JPG')
+# st.image(image, caption='sea hah',use_column_width=True)
+
+# if st.button('Say hello'):
+#   st.write('Why hello there')
+# else:
+#   st.write('Goodbye')
 
 '# 显示多选框'
 options = st.multiselect(
